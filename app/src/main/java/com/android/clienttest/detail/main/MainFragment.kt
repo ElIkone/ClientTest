5411f8db66df53d3c4b0420866f1fc3108eab464
Benjamin Munoz <elmaildebenja@gmail.com>
2021-11-05 22:52:06 -0600

    Feat: Add Main fragment layout

---
+++ app/src/main/java/com/android/clienttest/detail/main/MainFragment.kt
@@ -0,0 +1,17 @@
+package com.android.clienttest.detail.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.android.clienttest.R
+
+class MainFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+}
