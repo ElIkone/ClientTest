fcca0ae5301fb6c1d9eed49f234d14ccf18abb72
Benjamin Munoz <elmaildebenja@gmail.com>
2021-11-05 23:35:34 -0600

    Feat: Add users view model

---
+++ app/src/main/java/com/android/clienttest/viewModel/UsersViewModel.kt
@@ -0,0 +1,43 @@
+package com.android.clienttest.viewModel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.android.clienttest.model.RandomUserList
+import com.android.clienttest.network.NetworkRepository
+import kotlinx.coroutines.*
+
+class UsersViewModel constructor(private val mainRepository: NetworkRepository) : ViewModel() {
+    val errorMessage = MutableLiveData<String>()
+    val usersList = MutableLiveData<RandomUserList>()
+    var job: Job? = null
+    private val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
+        onError("Exception handled: ${throwable.localizedMessage}")
+    }
+    val loading = MutableLiveData<Boolean>()
+
+    fun getAllUsers() {
+        job = CoroutineScope(Dispatchers.IO + exceptionHandler).launch {
+            val response = mainRepository.getAllUsers()
+
+
+            withContext(Dispatchers.Main) {
+                if (response.results.isNotEmpty()) {
+                    usersList.postValue(response)
+                    loading.value = false
+                } else {
+                    onError("Error")
+                }
+            }
+        }
+    }
+
+    private fun onError(message: String) {
+        errorMessage.value = message
+        loading.value = false
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        job?.cancel()
+    }
+}
