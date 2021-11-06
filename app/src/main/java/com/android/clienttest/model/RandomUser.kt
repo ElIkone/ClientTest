96b0c3a98e81200de3776ed61959bb38da7c0dfe
Benjamin Munoz <elmaildebenja@gmail.com>
2021-11-05 23:09:14 -0600

    Feat: Add data class for  random user, update build gradle with kotlin parcelize

---
+++ app/src/main/java/com/android/clienttest/model/RandomUser.kt
@@ -0,0 +1,12 @@
+package com.android.clienttest.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class RandomUser(val gender: String, val name:Name, val picture:Picture, val email: String, val nat: String, val phone: String): Parcelable {
+    @Parcelize
+    data class Name(val title: String, val first: String, val last: String) : Parcelable
+    @Parcelize
+    data class Picture(val large: String) : Parcelable
+}
---
+++ app/src/main/java/com/android/clienttest/model/RandomUserList.kt
@@ -0,0 +1,3 @@
+package com.android.clienttest.model
+
+class RandomUserList(val results: List<RandomUser>)
