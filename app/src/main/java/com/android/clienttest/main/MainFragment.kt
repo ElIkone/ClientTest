d17c9a0a2ec173ef0390eaa95524ed97e8958d24
Benjamin Munoz <elmaildebenja@gmail.com>
2021-11-05 23:25:00 -0600

    Fix: Add adapter in main fragment

--- app/src/main/java/com/android/clienttest/main/MainFragment.kt
+++ app/src/main/java/com/android/clienttest/main/MainFragment.kt
@@ -8,9 +8,11 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.android.clienttest.R
 import com.android.clienttest.databinding.FragmentMainBinding
+import com.android.clienttest.model.RandomUser
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(), UserAdapter.UserItemListener {
     private lateinit var binding: FragmentMainBinding
+    private val adapter = UserAdapter(this)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -18,5 +20,9 @@ class MainFragment : Fragment() {
         binding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_main, container, false)
         return binding.root
+    }
+
+    override fun onClickedUser(userObject: RandomUser) {
+        TODO("Not yet implemented")
     }
 }
